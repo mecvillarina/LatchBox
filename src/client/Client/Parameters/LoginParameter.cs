4edcdc5d3b21a2676713c679e8b3f2d6f308c65d
Mark Erwin C. Villarina <me.villarina@live.com>
2022-04-16 16:58:31 +0800

    Added Login Partial

--- src/client/Client/Parameters/LoginParameter.cs
+++ src/client/Client/Parameters/LoginParameter.cs
@@ -1,6 +1,20 @@
-﻿namespace Client.Parameters
+﻿using FluentValidation;
+
+namespace Client.Parameters
 {
     public class LoginParameter
     {
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class LoginParameterValidator : AbstractValidator<LoginParameter>
+    {
+        public LoginParameterValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(v => v.Password)
+                .NotEmpty().WithMessage("Please input password");
+        }
     }
 }
