f1e2357fa4f9eb852c2b65610585c84600ffd8f2
Mark Erwin C. Villarina <me.villarina@live.com>
2022-04-17 22:26:11 +0800

    Added Locks Pages and Update Asset Page

---
+++ src/client/Client/Pages/Locks/MyLocksPage.razor.cs
@@ -0,0 +1,28 @@
+﻿using Client.Infrastructure.Extensions;
+using Client.Infrastructure.Models;
+using Client.Pages.Modals;
+using Client.Parameters;
+using MudBlazor;
+using Neo.SmartContract.Native;
+
+namespace Client.Pages.Locks
+{
+    public partial class MyLocksPage
+    {
+        public bool IsLoaded { get; set; }
+        public bool IsAssetLoaded { get; set; }
+
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await InvokeAsync(async () =>
+                {
+                    IsAssetLoaded = true;
+                    StateHasChanged();
+                });
+            }
+        }
+    }
+}
---
+++ src/client/Client/Pages/Locks/MyTokenRefundsPage.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Client.Pages.Locks
+{
+    public partial class MyTokenRefundsPage
+    {
+        public bool IsLoaded { get; set; }
+        public bool IsAssetLoaded { get; set; }
+
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await InvokeAsync(async () =>
+                {
+                    IsAssetLoaded = true;
+                    StateHasChanged();
+                });
+            }
+        }
+    }
+}
