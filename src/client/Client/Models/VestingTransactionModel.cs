ec66f685b352ec0d8166928b6773a281893b63be
Mark Erwin C. Villarina <me.villarina@live.com>
2022-04-25 13:08:59 +0800

    Added Vesting Previewer

---
+++ src/client/Client/Models/VestingTransactionModel.cs
@@ -0,0 +1,62 @@
+﻿using Client.Infrastructure.Extensions;
+using Client.Infrastructure.Models;
+using MudBlazor;
+using System.Numerics;
+
+namespace Client.Models
+{
+    public class VestingTransactionModel
+    {
+        private readonly BigInteger _totalAmount;
+
+        public AssetToken AssetToken { get; private set; }
+        public VestingTransaction Transaction { get; private set; }
+        public string InitiatorAddressDisplay { get; private set; }
+        public string TotalAmountDisplay { get; private set; }
+        public string DateCreationDisplay { get; private set; }
+        public string RevocableDisplay { get; private set; }
+        public string StatusDisplay { get; private set; }
+        public Color StatusDisplayColor { get; private set; }
+
+        public VestingTransactionModel(VestingTransaction transaction, AssetToken assetToken)
+        {
+            Transaction = transaction;
+            AssetToken = assetToken;
+
+            foreach (var receiver in transaction.Receivers)
+            {
+                _totalAmount += receiver.Amount;
+            }
+
+            InitiatorAddressDisplay = Transaction.InitiatorAddress;
+            DateCreationDisplay = Transaction.CreationTime.ToCurrentTimeZone().ToFormat("MMMM dd, yyyy hh:mm tt");
+            RevocableDisplay = Transaction.IsRevocable ? "Yes" : "No";
+            TotalAmountDisplay = $"{_totalAmount.ToAmount(AssetToken.Decimals).ToAmountDisplay(AssetToken.Decimals)} {AssetToken.Symbol}";
+
+            if (Transaction.IsActive)
+            {
+                if (Transaction.Periods.Any(x => DateTime.UtcNow < x.UnlockTime))
+                {
+                    StatusDisplay = "On Vesting";
+                    StatusDisplayColor = Color.Primary;
+                }
+                else
+                {
+                    StatusDisplay = "Unlocked";
+                    StatusDisplayColor = Color.Info;
+                }
+            }
+            else if (Transaction.IsRevoked)
+            {
+                StatusDisplay = "Revoked";
+                StatusDisplayColor = Color.Error;
+            }
+            else
+            {
+                StatusDisplay = "Completed";
+                StatusDisplayColor = Color.Info;
+            }
+
+        }
+    }
+}
